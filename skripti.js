e12828b3fb197d8cc11c89f2259e6b23d3e56886
Pichuks <122868731+Pichuks@users.noreply.github.com>
2023-02-07 10:46:15 +0200

    Create skripti.js

---
+++ skripti.js
@@ -0,0 +1,12 @@
+//alert("Čau")
+console.log("test")
+
+var logs = document.querySelector(".chataZinas");
+var zina = document.querySelector("#zina");
+var vards = document.querySelector("#vards");
+
+
+function sutitZinu(){
+    console.log("poga darbojas");
+    console.log("ziņu ir: "+zina.value);
+}
